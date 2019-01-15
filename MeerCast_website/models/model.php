d0db27a383807aa9f758a9d2f2cadd48a9b867ab
PtitMatcha <baiyuhe1314@gmail.com>
2019-01-15 18:27:58 +0100

    ajoutUserProperties

--- MeerCast_website/models/model.php
+++ MeerCast_website/models/model.php
@@ -4,7 +4,7 @@ function dbConnect()
 {
     try
     {
-        $db = new PDO('mysql:host=localhost;dbname=meercast;charset=utf8', 'root', '');
+        $db = new PDO('mysql:host=localhost;dbname=meercast;charset=utf8', 'root', 'root');
         return $db;
     }
 
@@ -94,20 +94,24 @@ function getCatalogue(){
 
 function getProperties()
 {
-        $db = dbConnect();
-        $req = $db->query("SELECT * FROM houses");
+    $db = dbConnect();
+    $req = $db->prepare("SELECT * FROM houses WHERE id_user = :id");
+    $req -> execute(array("id"=>$_SESSION["id"]));
 
-        return $req;
+
+    return $req;
 }
 
 function insertProperty($property_name, $property_type)
 {
     $db = dbConnect();
 
-    $req = $db->prepare("INSERT INTO houses(property_name, property_type) VALUES(:property_name, :property_type)");
+    $req = $db->prepare("INSERT INTO houses(property_name, property_type, id_user) VALUES(:property_name, :property_type, :id_user)");
 
     $req->bindParam("property_name", $property_name);
     $req->bindParam("property_type", $property_type);
+    $req->bindParam("id_user", $_SESSION["id"]);
+
 
     $req->execute();
     $req->closeCursor();
