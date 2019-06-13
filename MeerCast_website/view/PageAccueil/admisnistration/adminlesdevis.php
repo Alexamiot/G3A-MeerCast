936a898b2315bdce18d3106e78eab4f8c427fa91
Alexandre <44364111+Alexamiot@users.noreply.github.com>
2019-06-13 11:05:55 +0200

    changement design des devis

--- MeerCast_website/view/PageAccueil/admisnistration/adminlesdevis.php
+++ MeerCast_website/view/PageAccueil/admisnistration/adminlesdevis.php
@@ -12,7 +12,8 @@
 </head>
 
 <body>
-  
+
+<div id="lesDevis">
      <?php  foreach ($devis as $devi) {
 ?>
  
@@ -38,9 +39,8 @@ $type=$devi["type"];
 
 $comment=$devi["comment"];
 
-         $alarm=  $devi["alarm"]; 
-        
-      $elec=  $devi["elec"]; 
+     $alarm=  $devi["alarm"];
+     $elec=  $devi["elec"];
       $heater =$devi["heater"];
       $AC=  $devi["AC"]; 
       $pool =$devi["pool"];
@@ -49,93 +49,86 @@ $comment=$devi["comment"];
       $devices=  $devi["devices"]; 
       $shutters =$devi["shutters"];
       $garden =$devi["garden"];
-      ?> 
-<div id="devisinfo">  
-  <div>
-
-              <div class="flip-card">
-              <div class="flip-card-inner">
-                <div class="flip-card-front">
-                 <?php if($gender=="M") { ?><img src="view/Design/imagesMaison/manIcon.png" alt="Avatar" style="width:200px;height:200px;"> <?php }else { ?><img src="view/Design/imagesMaison/womanIcon.png" alt="Avatar" style="width:200px;height:200px;"> <?php } ?>
-                </div>
-                <div class="flip-card-back">
-                  <h4><?php echo $type.": " ?></h4>
-                  <h1><?php echo $name ." ". $last_name ?></h1> 
-                  <p><?php echo $email ?></p> 
-                  <br/>
-                  <p><?php echo $tel."".$phonenumber ?></p>
-                  <br/>
-                  <p><?php echo $adress." ".$adress2 ?></p>
-                  <br/>
-                  <p><?php echo $city." ".$areacode." ".$country ?></p>
-                  
+      ?>
 
 
+    <div id="devisinfo">
+      <div>
+          <h2 style="text-align: center"><?php echo $name ." ". $last_name ?></h2>
+                  <div class="flip-card">
+                  <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                     <?php if($gender=="M") { ?><img src="view/Design/imagesMaison/manIcon.png" alt="Avatar" style="width:100%;height:100%;"> <?php }else { ?><img src="view/Design/imagesMaison/womanIcon.png" alt="Avatar" style="width:100%;height:100%;"> <?php } ?>
+                    </div>
+                    <div class="flip-card-back">
+                      <h4><?php echo $type.": " ?></h4>
+                      <h1><?php echo $name ." ". $last_name ?></h1>
+                      <p><?php echo $email ?></p>
+                      <br/>
+                      <p><?php echo $tel."".$phonenumber ?></p>
+                      <br/>
+                      <p><?php echo $adress." ".$adress2 ?></p>
+                      <br/>
+                      <p><?php echo $city." ".$areacode." ".$country ?></p>
 
 
+                    </div>
+                  </div>
 
-                </div>
-              </div>
-
-            </div>
-
-
-
-
-  </div>
-
-
-
-  <div >
-             <label> <br>
-                <select name="country">
-                    
-                        <option value="">Les options choisies sont :</option>
-                        <?php if($alarm=="oui" ){ ?><option value= "<?php echo $alarm ?>"> "arlam :" </option> <?php } ?> 
-                        <?php if($elec=="oui" ){?><option value="<?php echo $elec ?>">"elec :"<?php echo $elec ?></option><?php } ?>
-                        <?php if($AC=="oui" ){?><option value="<?php echo $AC ?>">"AC :"<?php echo $AC ?></option><?php } ?>
-                        <?php if($pool=="oui" ){?><option value= "<?php echo $pool ?>"> "pool :" <?php echo $pool ?></option><?php } ?>
-                        <?php if($gate=="oui" ){?><option value="<?php echo $gate ?>">"gate :"<?php echo $gate ?></option><?php } ?>
-                       <?php if($lighting=="oui" ){ ?><option value="<?php echo $lighting ?>">"lighting :"<?php echo $lighting ?></option><?php } ?>
-                        <?php if($devices=="oui" ){?><option value= "<?php echo $devices ?>"> "devices :" <?php echo $devices ?></option><?php } ?>
-                       <?php if($shutters=="oui" ){?> <option value="<?php echo $shutters ?>">"shutters :"<?php echo $shutters ?></option><?php } ?>
-                       <?php if($garden=="oui" ){?> <option value="<?php echo $garden ?>">"garden :"<?php echo $garden ?></option><?php } ?>
-                    
-              
-                </select>
-            </label> 
-  </div>
-  <div>   
-               <button class="myBtne">Other info</button>
-
-              <!-- The Modal -->
-              <div class="myModale modal">
-
-                <!-- Modal content -->
-                <div class="modal-content">
-                  <span class="close">&times;</span>
-                  <p><?php echo "type de batiment est concerné : ".$building ?></p>
-                  <br/>
-                  <p><?php echo "type de construction : ".$construction ?></p>
-                  <br/>
-                  <p><?php echo "La surface est  : ".$surface ?></p>
-                  <br/>
-                  <p><?php echo "Commentaire:   : ".$comment ?></p>
-                  
-                  <br/>
                 </div>
 
+        </div>
 
-              </div>
-  </div>
-  
+      <div>
+                 <label> <br>
+                    <select style="width: 100%;" name="country">
 
-  
+                            <option  value="">Les options choisies sont :</option>
+                            <?php if($alarm=="oui" ){ ?><option value= "<?php echo $alarm ?>"> Alarme </option> <?php } ?>
+                            <?php if($elec=="oui" ){?><option value="<?php echo $elec ?>">chauffage électrique</option><?php } ?>
+                            <?php if($AC=="oui" ){?><option value="<?php echo $AC ?>">Climatisation</option><?php } ?>
+                            <?php if($pool=="oui" ){?><option value= "<?php echo $pool ?>"> Piscine </option><?php } ?>
+                            <?php if($gate=="oui" ){?><option value="<?php echo $gate ?>">Portail</option><?php } ?>
+                           <?php if($lighting=="oui" ){ ?><option value="<?php echo $lighting ?>">Eclairage</option><?php } ?>
+                            <?php if($devices=="oui" ){?><option value= "<?php echo $devices ?>"> Electroménager </option><?php } ?>
+                           <?php if($shutters=="oui" ){?> <option value="<?php echo $shutters ?>">Volets</option><?php } ?>
+                           <?php if($garden=="oui" ){?> <option value="<?php echo $garden ?>">Jardin</option><?php } ?>
 
-</div>
+
+                    </select>
+                </label>
+      </div>
+      <div>
+                   <button  class="myBtne">Other info</button>
+
+                  <!-- The Modal -->
+                  <div class="myModale modal">
+
+                    <!-- Modal content -->
+                    <div class="modal-content">
+                      <span class="close">&times;</span>
+                      <p><?php echo "type de batiment est concerné : ".$building ?></p>
+                      <br/>
+                      <p><?php echo "type de construction : ".$construction ?></p>
+                      <br/>
+                      <p><?php echo "La surface est  : ".$surface ?></p>
+                      <br/>
+                      <p><?php echo "Commentaire:   : ".$comment ?></p>
+
+                      <br/>
+                    </div>
+
+
+                  </div>
+      </div>
+
+
+
+
+    </div>
 
     <?php }  ?>
-
+</div>
 
 
 
